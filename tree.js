d68e7e8b64b0c8aadcaee99bccdcca8f26a5084a
clyx <chelvyixue@gmail.com>
2017-01-03 00:19:38 +0800

    tree switch example

--- tree.js
+++ tree.js
@@ -1,4 +1,4 @@
-function printTree(root, indent) {
+function printTree(root, indent=0) {
     if (!root) return;
     for (var i = 0; i < indent; i++) {
         document.getElementById("tree").innerHTML += ".&nbsp";
@@ -8,26 +8,58 @@ function printTree(root, indent) {
         document.getElementById("tree").innerHTML += "<span class=\"leaf\">" + root + "</span><br />";
     }
     else {
-        document.getElementById("tree").innerHTML += root.ruleIndex.toString() + "<br />";
+        document.getElementById("tree").innerHTML += root + "<br />";
     }
     for (i = 0; i < n; i++) {
+        if (root.ruleIndex == 83)
+            console.log(root.getChild(0).symbol);
         printTree(root.getChild(i), indent + 1);
     }
 }
 
+function parseTree(root) {
+    if (!root) return;
+    var n = root.getChildCount();
+    if (n == 0) { // Leaf node
+
+    }
+    else {
+        var ruleIndex = root.ruleIndex;
+        switch (ruleIndex) {
+            case 83:    // integer
+                return Data(Data.t_integer, parseInt(root.getChild(0).symbol.text));
+                break;
+            case 82:    // number
+                return parseTree(root.getChild(0));
+                break;
+            case 81:    // string
+                return Data(Data.t_string, root.getChild(0).symbol.text);
+                break;
+        }
+    }
+}
+
+function Data(type, val) {
+    this.type = type;
+    this.val = val;
+}
+
+Data.t_integer = 0;
+
 var antlr4 = require('antlr4/index');
-var FPythonLexer = require('FPythonLexer');
-var FPythonParser = require('FPythonParser');
+var FPython1Lexer = require('FPython1Lexer');
+var FPython1Parser = require('FPython1Parser');
 document.getElementById("parse").addEventListener("click", function(){
     var input = document.getElementById("code").value;
     input = SolveIndent(input);
     var chars = new antlr4.InputStream(input);
-    var lexer = new FPythonLexer.FPythonLexer(chars);
+    var lexer = new FPython1Lexer.FPython1Lexer(chars);
     var tokens  = new antlr4.CommonTokenStream(lexer);
-    var parser = new FPythonParser.FPythonParser(tokens);
+    var parser = new FPython1Parser.FPython1Parser(tokens);
     parser.buildParseTrees = true;
     var tree = parser.file_input();
     console.log(tree);
-    printTree(tree, 0);
+    printTree(tree);
+    //parseTree(tree);
 });
     
